7265145feac1ca431504fd8137c706a0349db8e5
Mohit Bansal <mohit.bansal@gep.com>
2020-11-02 03:24:02 +0530

    Update atandt.py

--- atandt.py
+++ atandt.py
@@ -1,5 +1,5 @@
 import outlook
 mail = outlook.Outlook()
-mail.login('emailaccouqwqwnt@live.com','wewew dsword')
+mail.login('emailaccouqwqwnt@live.com','wewew 2 reviwerdsword')
 mail.inbox()
 print mail.unread()
